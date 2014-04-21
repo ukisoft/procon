65bec37b35525a5175bcc3edd0e92f8ea1a93499
ukisoft <msorakubo@msn.com>
2014-04-22 00:42:50 +0900

    Refactor 0009

--- 0009.php
+++ 0009.php
@@ -12,17 +12,11 @@ class NumberMagicEasy {
         $cards = [[1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 9, 10, 11, 12], [1, 2, 5, 6, 9, 10, 13, 14], [1, 3, 5, 7, 9, 11, 13, 15]];
         $base = range(1, 16);
 
-        $aLotOfClue = [];
         foreach (str_split($answer) as $answerNo => $oneAnswer) {
-            if ($oneAnswer === 'Y') $oneClue = $cards[$answerNo];
-            else $oneClue = array_diff($base, $cards[$answerNo]);
-            foreach ($oneClue as $num) {
-                $aLotOfClue[] = $num;
-            }
+            if ($oneAnswer === 'Y') $base = array_diff($base, array_diff($base, $cards[$answerNo]));
+            else $base = array_diff($base, $cards[$answerNo]);
         }
 
-        foreach (array_count_values($aLotOfClue) as $num => $count) {
-            if ($count === 4) return $num;
-        }
+        return array_shift($base);
     }
 }
