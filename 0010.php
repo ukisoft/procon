c4e8fb9649789b4d54a89db62c7c5b858c0d4b9b
ukisoft <msorakubo@msn.com>
2014-05-01 15:06:08 +0900

    refactor 10

--- 0010.php
+++ 0010.php
@@ -5,6 +5,8 @@ namespace Procon;
 class CorporationSalary
 {
 
+    private $salaries = [];
+
     public function totalSalary($relations)
     {
 
@@ -14,37 +16,51 @@ class CorporationSalary
         // 給与の計算は、$salaryにある値と、自分のYのカラムを使って計算する。
         // $salaryのカウントが$relationsと同じになったら終了して、$salaryの総額を返す
 
-        $salaries = [];
         $relationsCountNum = count($relations);
 
-        while (count($salaries) < $relationsCountNum) { // 給与が全部埋まるまでループ
-            foreach ($relations as $personNo => $manageListString) {
-
-                if (array_key_exists($personNo, $salaries)) { // すでに給与が計算済みならスキップ
-                    continue;
-                }
-
-                $manageList = str_split($manageListString);
-                if (in_array('Y', $manageList) === false) { // 全部Nなら給与は1
-                    $salaries[$personNo] = 1;
-                    continue;
-                }
-
-                // $manageLstにYがあり、且つ給与計算済みでなければスキップ。そうでなければ計算
-                $salary = 0;
-                foreach ($manageList as $manageTarget => $manageStatus) {
-                    if ($manageStatus === 'Y') {
-                        if (array_key_exists($manageTarget, $salaries) === false) {
-                            continue 2;
-                        } else {
-                            $salary += $salaries[$manageTarget];
-                        }
-                    }
-                }
-                $salaries[$personNo] = $salary;
-            }
+        while (count($this->salaries) < $relationsCountNum) { // 給与が全部埋まるまでループ
+            $this->addSalaryPossibly($relations);
         }
 
-        return array_sum($salaries);
+        return array_sum($this->salaries);
+    }
+
+    private function addSalaryPossibly($relations)
+    {
+        foreach ($relations as $personNo => $manageListString) {
+
+            if (array_key_exists($personNo, $this->salaries)) { // すでに給与が計算済みならスキップ
+                continue;
+            }
+
+            $manageList = str_split($manageListString);
+            if (in_array('Y', $manageList) === false) { // 全部Nなら給与は1
+                $this->salaries[$personNo] = 1;
+                continue;
+            }
+
+            // $manageLstにYがあり、且つ給与計算済みでなければスキップ。そうでなければ計算
+            try {
+                $this->salaries[$personNo] = $this->calculateSalary($manageList);
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+                continue;
+            }
+        }
+    }
+
+    private function calculateSalary($manageList)
+    {
+        $salary = 0;
+        foreach ($manageList as $manageTarget => $manageStatus) {
+            if ($manageStatus === 'Y') {
+                if (array_key_exists($manageTarget, $this->salaries) === false) {
+                    throw new \Exception('給与未計算');
+                } else {
+                    $salary += $this->salaries[$manageTarget];
+                }
+            }
+        }
+        return $salary;
     }
 }
