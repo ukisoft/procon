6bb6d58a93c80dc6b43bec88b71e6aa2eac5c530
ukisoft <msorakubo@msn.com>
2014-05-21 21:54:09 +0900

    refactor 0011

--- 0011.php
+++ 0011.php
@@ -4,52 +4,29 @@ namespace Procon;
 
 class BadNeighbors
 {
-    private $donations;
-    private $memo;
-
     public function maxDonations($donations)
     {
-        // [['0100' => 5], ['0101' => 8]...]
-
-        // bit演算でパターン作る
-        // foreachで回す
-        // 隣人チェックで隣になったパターンをフィルタする
-        // foreachでcalcDonationを呼ぶ
-        // 計算したdonationは、メモに格納する
-
-        $this->donations = $donations;
-        $this->memo = [];
-        $numberOfPeople = count($donations);
-        $patterns = [];
-
-        for ($i = 1; $i < pow(2, $numberOfPeople); $i++) {
-            $pattern = sprintf('%0' . (String)$numberOfPeople . 'b', $i);
-            if (preg_match('/^1[01]*1$|11/', $pattern) === 0) {
-                $patterns[] = $pattern;
-            }
-        }
-
-        $result = [];
-        foreach ($patterns as $pattern) {
-            $result[] = $this->calcDonation($pattern);
-        }
-
-        return max($result);
+        $copiedDonations = $donations;
+        array_shift($copiedDonations);
+        array_pop($donations);
+        return max($this->calcLineDonation($copiedDonations), $this->calcLineDonation($donations));
     }
 
-    private function calcDonation($pattern)
+    private function calcLineDonation($lineDonations)
     {
-        if (array_key_exists($pattern, $this->memo)) {
-            return $this->memo[$pattern];
+        $dp = [];
+        foreach ($lineDonations as $numberOfPerson => $donation) {
+            if ($numberOfPerson < 2) {
+                $dp[$numberOfPerson] = $lineDonations[$numberOfPerson];
+                continue;
+            } elseif ($numberOfPerson === 2) {
+                $dp[$numberOfPerson] = $lineDonations[$numberOfPerson] + $dp[0];
+                continue;
+            } else {
+                $dp[$numberOfPerson] = $lineDonations[$numberOfPerson]
+                                       + max($dp[$numberOfPerson - 2], $dp[$numberOfPerson - 3]);
+            }
         }
-        $firstOnePosition = strpos($pattern, '1');
-        if (array_sum(str_split($pattern)) === 1) {
-            $donation = $this->donations[$firstOnePosition];
-            $this->memo[$pattern] = $donation;
-            return $donation;
-        }
-        $donation = $this->calcDonation(substr($pattern, 0, $firstOnePosition) . '0' . substr($pattern, $firstOnePosition + 1)) + $this->donations[$firstOnePosition];
-        $this->memo[$pattern] = $donation;
-        return $donation;
+        return max($dp);
     }
 }
