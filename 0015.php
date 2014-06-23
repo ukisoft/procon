0c74b9b9d0c9246660ff8c34bda9565640502474
ukisoft <msorakubo@msn.com>
2014-06-23 15:58:25 +0900

    fin 0015

--- 0015.php
+++ 0015.php
@@ -4,6 +4,9 @@ namespace Procon;
 
 class StockHistory
 {
+    private $KEY_BENEFIT = 'benefit';
+    private $KEY_STOCK_PRICE = 'stockPrice';
+
     public function maximumEarnings($initialInvestment, $monthlyContribution, $stockPrices)
     {
         /*
@@ -17,24 +20,8 @@ class StockHistory
          */
 
         $companyStockFlows = $this->parseStockPrices($stockPrices);
-
-        // メソッド化?
-        $maxEarnings = [];
-        foreach ($companyStockFlows as $companyStockFlow) {
-            $maxEarnings[] = $this->getMaxEarningOfCompany($companyStockFlow);
-        }
-
-        arsort($maxEarnings);
-        $benefit = 0;
-        foreach ($maxEarnings as $companyNo => $maxEarning) {
-            if ($maxEarning === 0) {
-                break;
-            }
-            $benefit += ($initialInvestment / $companyStockFlows[$companyNo][0]) * $maxEarning;
-            $initialInvestment = $initialInvestment % $companyStockFlows[$companyNo][0];
-        }
-
-        return $benefit;
+        $moneyFlow = $this->getMoneyFlow($initialInvestment, $monthlyContribution, count($companyStockFlows[0]));
+        return $this->getBenefit($companyStockFlows, $moneyFlow);
     }
 
     private function parseStockPrices($stockPrices)
@@ -61,12 +48,46 @@ class StockHistory
         return $result;
     }
 
-    private function getMaxEarningOfCompany($companyStockFlow)
+    private function getMaxEarningOfCompany($companyStockFlow, $from)
     {
         $result = [];
-        for ($i = 0; $i < count($companyStockFlow); $i++) {
-            $result[] = $companyStockFlow[$i] - $companyStockFlow[0];
+        for ($i = $from; $i < count($companyStockFlow); $i++) {
+            $result[] = [$this->KEY_BENEFIT => $companyStockFlow[count($companyStockFlow) - 1] - $companyStockFlow[$i], $this->KEY_STOCK_PRICE => $companyStockFlow[$i]];
         }
-        return max($result);
+
+        foreach ($result as $buyTiming => $earning) {
+            $sort_key[$buyTiming] = $earning[$this->KEY_BENEFIT];
+        }
+        array_multisort($sort_key, SORT_DESC, $result);
+
+        return $result[0];
+    }
+
+    private function getMoneyFlow($initialInvestment, $monthlyContribution, $term)
+    {
+        $moneyFlow = [$initialInvestment];
+        for ($i = 1; $i < $term; $i++) {
+            $moneyFlow[] = $monthlyContribution;
+        }
+        return $moneyFlow;
+    }
+
+    private function getBenefit($companyStockFlows, $moneyFlow)
+    {
+        $benefit = 0;
+        for ($i = 0; $i < count($moneyFlow); $i++) {
+            $maxEarnings = [];
+            foreach ($companyStockFlows as $companyStockFlow) {
+                $maxEarnings[] = $this->getMaxEarningOfCompany($companyStockFlow, $i);
+            }
+
+            foreach ($maxEarnings as $companyNo => $maxEarning) {
+                $sort_key[$companyNo] = $maxEarning[$this->KEY_BENEFIT];
+            }
+            array_multisort($sort_key, SORT_DESC, $maxEarnings);
+
+            $benefit += ($moneyFlow[$i] / $maxEarnings[0][$this->KEY_STOCK_PRICE]) * $maxEarnings[0][$this->KEY_BENEFIT];
+        }
+        return round($benefit);
     }
 }
