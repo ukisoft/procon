9065a8c1f42098cd7900c1db31589674f0ca294a
空久保充弘 <msorakubo@msn.com>
2014-07-10 15:26:40 +0900

    add SelectionSort

---
+++ SelectionSort.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ProCon;
+
+class SelectionSort
+{
+    static public function sortInt($array)
+    {
+        foreach ($array as $key => &$num) {
+            if (is_numeric($num) == false) throw new InvalidArgumentException("引数の配列は、intのみ格納してください。");
+            $tmp = [$key, $num];
+            for ($i = $key; $i < count($array); $i++) {
+                if ($tmp[1] > $array[$i]) $tmp = [$i, $array[$i]];
+            }
+            if ($key !== $tmp[0]) {
+                $array[$tmp[0]] = $num;
+                $array[$key] = $tmp[1];
+            }
+        }
+        return $array;
+    }
+}
---
+++ SelectionSortTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Procon;
+
+require_once('SelectionSort.php');
+
+class SelectionSortTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function testSortInt($array, $expected)
+    {
+        $actual = SelectionSort::sortInt($array);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function provider()
+    {
+        return [[[1, 3, 2, 5], [1, 2, 3, 5]],
+            [[3, 3, 5, 1], [1, 3, 3, 5]]];
+    }
+}
