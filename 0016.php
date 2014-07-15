0d60059f54e18a2bf3e6d7ec74ec6046c7c7ebb7
空久保充弘 <msorakubo@msn.com>
2014-07-15 11:07:21 +0900

    refactor 0016 and SelectionSort

--- 0016.php
+++ 0016.php
@@ -6,8 +6,70 @@ class BatchSystem
 {
     public function schedule($duration, $user)
     {
-        $taskSet = array_map(function ($oneDurationKey, $oneDuration, $oneUser) {return ["num" => $oneDurationKey, "duration" => $oneDuration, "user" => $oneUser];}, array_keys($duration), array_values($duration), $user);
-        array_multisort($duration, $user, $taskSet);
-        return array_map(function ($task) {return $task["num"];}, $taskSet);
+        $users = [];
+        array_map(function ($oneDuration, $oneUser, $userKey) use (&$users) {
+            foreach ($users as $user) {
+                if ($user->getName() == $oneUser) {
+                    $user->addNewTask($oneDuration, $userKey);
+                    return;
+                }
+            }
+            $users[] = new User($oneDuration, $oneUser, $userKey);
+        }, $duration, array_values($user), array_keys($user));
+
+        $sortAverageDuration = [];
+        $sortName = [];
+        $sortPosition = [];
+        foreach ($users as $oneUser) {
+            $sortAverageDuration[] = $oneUser->getAverageDuration();
+            $sortName[] = $oneUser->getName();
+            $sortPosition[] = $oneUser->getPosition();
+        }
+
+        array_multisort($sortAverageDuration, $sortName, $sortPosition);
+        $result = [];
+        foreach ($sortPosition as $oneUserPosition) {
+            foreach ($oneUserPosition as $onePosition) {
+                $result[] = $onePosition;
+            }
+        }
+
+        return $result;
+    }
+}
+
+class User
+{
+    private $name;
+    private $totalDuration = 0;
+    private $position = [];
+
+    public function __construct($duration, $name, $position)
+    {
+        $this->name = $name;
+        $this->totalDuration += $duration;
+        $this->position[] = $position;
+    }
+
+    public function addNewTask($duration, $position)
+    {
+        $this->totalDuration += $duration;
+        $this->position[] = $position;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAverageDuration()
+    {
+        return $this->totalDuration / count($this->position);
+    }
+
+    public function getPosition()
+    {
+        asort($this->position);
+        return $this->position;
     }
 }
--- SelectionSort.php
+++ SelectionSort.php
@@ -8,14 +8,16 @@ class SelectionSort
     {
         foreach ($array as $key => &$num) {
             if (is_numeric($num) == false) throw new InvalidArgumentException("引数の配列は、intのみ格納してください。");
-            $tmp = [$key, $num];
+            $minKey = $key;
+            $minNum = $num;
             for ($i = $key + 1; $i < count($array); $i++) {
-                if ($tmp[1] > $array[$i]) $tmp = [$i, $array[$i]];
+                if ($minNum > $array[$i]) {
+                    $minKey = $i;
+                    $minNum = $array[$i];
+                }
             }
-            if ($key !== $tmp[0]) {
-                $array[$tmp[0]] = $num;
-                $array[$key] = $tmp[1];
-            }
+            $array[$minKey] = $num;
+            $array[$key] = $minNum;
         }
         return $array;
     }
