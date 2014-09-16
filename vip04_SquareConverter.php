ffa545278f8c720b76a16cd55a43a46212054c44
空久保充弘 <msorakubo@msn.com>
2014-09-16 20:31:22 +0900

    SquareConvertを追加 ただし、2しか動かず・・・

---
+++ vip04_SquareConverter.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * 与えられた数の平方根を求める
+ * 当然ライブラリは使わない
+ */
+
+namespace Vip;
+
+class SquareConverter {
+
+    static private $MAX_DIGIT = 6;
+
+    static public function get($num)
+    {
+        $intSquareDigit = SquareConverter::findIntSquare($num);
+        if (strlen((string)$intSquareDigit) >= SquareConverter::$MAX_DIGIT) {
+            return $intSquareDigit;
+        }
+        $intSquareDigit = (string)$intSquareDigit . '.';
+        while (true) {
+            $intSquareDigit = SquareConverter::getMorePreciousSquare($num, $intSquareDigit);
+            if (strlen((string)$intSquareDigit) > SquareConverter::$MAX_DIGIT) {
+                return $intSquareDigit;
+            }
+        }
+    }
+
+    static private function findIntSquare($num)
+    {
+        $intSquareDigit = 0;
+        while (true) {
+            if ($num < $intSquareDigit * $intSquareDigit) {
+                return --$intSquareDigit;
+            }
+            $intSquareDigit++;
+        }
+    }
+
+    static private function getMorePreciousSquare($num, $previousNum)
+    {
+        $currentNum = 9;
+        while (true) {
+            $newNum = (double)((string)$previousNum . (string)$currentNum);
+            if ($num > $newNum * $newNum) {
+                return $newNum;
+            }
+            $currentNum--;
+        }
+    }
+}
