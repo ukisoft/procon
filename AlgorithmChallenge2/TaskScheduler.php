381f68626db500822f35e5b4d702d229531f4c0d
空久保充弘 <msorakubo@msn.com>
2014-11-19 09:53:51 +0900

    TaskSchedulerの処理方法を、先にソートしてからに修正

--- AlgorithmChallenge2/TaskScheduler.php
+++ AlgorithmChallenge2/TaskScheduler.php
@@ -6,29 +6,17 @@ class TaskScheduler
 {
     public static function findMaxTaskCount($startTimes, $endTimes)
     {
+        array_multisort($startTimes, SORT_ASC, $endTimes);
+        sort($endTimes);
+
         $taskCount = 0;
-        // タスクをこなすたびに、startTimesを減らしていくので、評価式はstartTimesの数。
-        while (count($startTimes) > 0) {
-            // 残っている最小のstartTimesに対応したKeyを取得する。
-            $nextTaskKey = 100001;
-            $nextStartTime = 100000001;
-            foreach ($startTimes as $key => $startTime) {
-                if ($startTime < $nextStartTime) {
-                    $nextTaskKey = $key;
-                    $nextStartTime = $startTime;
-                }
+        $lastEndTime = 0;
+        foreach ($endTimes as $key => $endTime) {
+            if ($lastEndTime < $startTimes[$key]) {
+                $lastEndTime = $endTime;
+                $taskCount++;
             }
-            // 対応する終了時間を退避。
-            $lastEndTime = $endTimes[$nextTaskKey];
-            $taskCount++;
-            // 終了時間よりも前に始まるタスクを削除する。
-            $startTimes = array_filter($startTimes, function($startTime) use ($lastEndTime) {
-                if ($startTime > $lastEndTime) {
-                    return $startTime;
-                }
-            });
         }
         return $taskCount;
     }
-
 } 
