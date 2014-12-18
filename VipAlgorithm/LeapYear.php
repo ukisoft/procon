47ba1855f6b529c7a11df84041b9f913c307703c
空久保充弘 <msorakubo@msn.com>
2014-12-18 19:29:26 +0900

    LeapYear実装

--- VipAlgorithm/LeapYear.php
+++ VipAlgorithm/LeapYear.php
@@ -6,6 +6,15 @@ class LeapYear
 {
     public static function solve($year)
     {
-        return true;
+        if ($year % 400 === 0) {
+            return true;
+        }
+        if ($year % 100 === 0) {
+            return false;
+        }
+        if ($year % 4 === 0) {
+            return true;
+        }
+        return false;
     }
 } 
