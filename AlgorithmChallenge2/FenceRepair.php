5e747f9962132cd747005e735e6630bb71d73dc3
空久保充弘 <msorakubo@msn.com>
2015-01-13 11:22:37 +0900

    SplPriorityQueueを使ってリファクタ

--- AlgorithmChallenge2/FenceRepair.php
+++ AlgorithmChallenge2/FenceRepair.php
@@ -12,25 +12,16 @@ class FenceRepair
          * すべて足し終えた時の、これまで足し算した全ての値の合計が最小コスト
          */
         $cost = 0;
-        asort($boardsLength);
-        while (count($boardsLength) > 1) {
-            $firstBoard = array_shift($boardsLength);
-            $secondBoard = array_shift($boardsLength);
+        $heap = new \SplPriorityQueue();
+        foreach ($boardsLength as $boardLength) {
+            $heap->insert($boardLength, 1 / $boardLength);
+        }
+        while ($heap->count() > 1) {
+            $firstBoard = $heap->extract();
+            $secondBoard = $heap->extract();
             $cost += $newBoard = $firstBoard + $secondBoard;
-            $boardsLength = FenceRepair::insertNumToArrayThroughAsort($newBoard, $boardsLength);
+            $heap->insert($newBoard, 1 / $newBoard);
         }
         return $cost;
-    }
-
-    private static function insertNumToArrayThroughAsort($num, $array)
-    {
-        foreach ($array as $key => $value) {
-            if ($value >= $num) {
-                array_splice($array, $key, 0, $num);
-                return $array;
-            }
-        }
-        array_push($array, $num);
-        return $array;
     }
 }
