bbd4b7bc359434cca0e8fbe4af9481e06eaea950
空久保充弘 <msorakubo@msn.com>
2015-05-21 17:30:21 +0900

    Knapsack2を実装（メモリエラー）

--- AlgorithmChallenge2/Knapsack2.php
+++ AlgorithmChallenge2/Knapsack2.php
@@ -2,10 +2,38 @@
 
 namespace ProCon;
 
+require_once('Knapsack.php');
+
 class Knapsack2
 {
     public static function calc($items, $knapsack)
     {
-        return 1;
+        $items = array_map(function($item) {
+            return new Item($item[0], $item[1]);
+        }, $items);
+
+        $note = [(int)$knapsack => 0];
+        while (list($key, $item) = each($items)) {
+            $nextNote = [];
+            while (list($knapsackSpace, $value) = each($note)) {
+                $nextKnapsackSpace = $knapsackSpace - $item->weight;
+                if ($nextKnapsackSpace < 0) {
+                    continue;
+                }
+                if (array_key_exists($nextKnapsackSpace, $note)) {
+                    $nextNote[$nextKnapsackSpace] = max($value + $item->value, $note[$nextKnapsackSpace]);
+                    continue;
+                }
+                $nextNote[$nextKnapsackSpace] = $value + $item->value;
+            }
+            reset($note);
+            while (list($key, $value) = each($note)) {
+                if (array_key_exists($key, $nextNote) === false) {
+                    $nextNote[$key] = $value;
+                }
+            }
+            $note = $nextNote;
+        }
+        return max($note);
     }
 }
