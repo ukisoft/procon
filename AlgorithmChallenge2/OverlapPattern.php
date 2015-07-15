e98cd9241395742da9a30e114b4ae629b5229e9a
空久保充弘 <msorakubo@msn.com>
2015-07-15 19:02:04 +0900

    bug fix and refactor

--- AlgorithmChallenge2/OverlapPattern.php
+++ AlgorithmChallenge2/OverlapPattern.php
@@ -9,16 +9,14 @@ class OverlapPattern
         /**
          * f($n, $m, $a) = f($n - 1, $m, $a[1:]) + f($n -1, $m - 1, $a[1:]) + ... + f($n -1, 0, $a[1:])
          */
+        if ($n === 1 && $a[0] < $m) {
+            return 0;
+        }
         if ($n === 1) {
             return 1;
         }
-        $result = 0;
         $note = [];
-        $limit = array_shift($a);
-        for ($i = 0; $i <= min($m, $limit); $i++) {
-            $result += OverlapPattern::calcPartial($n - 1, $m - $i, $a, $M, $note) % $M;
-        }
-        return $result % $M;
+        return OverlapPattern::calcPartial($n, $m, $a, $M, $note) % $M;
     }
 
     private static function calcPartial($n, $m, $a, $M, &$note)
