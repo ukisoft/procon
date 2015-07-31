b16abd0e2c8b330548c259d2910d7548927e7f9f
空久保充弘 <msorakubo@msn.com>
2015-07-31 19:31:55 +0900

    add test

--- AlgorithmChallenge2_2_data_structure/2_fence_repair.py
+++ AlgorithmChallenge2_2_data_structure/2_fence_repair.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import heapq
+from datetime import datetime
 
 
 def solve(N, L):
@@ -13,5 +14,10 @@ def solve(N, L):
     return result
 
 if __name__ == '__main__':
+    print(datetime.now())
     print(solve(3, [8, 5, 8]) == 34)
     print(solve(20000, [50000 for i in range(20000)]))
+    print(solve(4, [8, 6, 5, 7]) == 52)
+    print(solve(4, [1, 8, 1, 1]) == 16)
+    print(solve(1, [1]) == 0)
+    print(datetime.now())
