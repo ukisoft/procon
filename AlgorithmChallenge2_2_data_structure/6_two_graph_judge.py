2304eef61109d9e196f0c8f90b935290cc04d387
空久保充弘 <msorakubo@msn.com>
2015-10-14 18:45:40 +0900

    edge -> node（誤字）

--- AlgorithmChallenge2_2_data_structure/6_two_graph_judge.py
+++ AlgorithmChallenge2_2_data_structure/6_two_graph_judge.py
@@ -13,26 +13,26 @@ def solve(n, chains):
     chain_table = {i: {j: 0 for j in range(n)} for i in range(n)}
     for first, second in chains:
         chain_table[first][second] = chain_table[second][first] = 1
-    for edge_no, chain_row in chain_table.items():
-        for _edge_no, step_sts in chain_row.items():
+    for node_no, chain_row in chain_table.items():
+        for _node_no, step_sts in chain_row.items():
             if step_sts == 1:
-                chain_table[edge_no][_edge_no] = chain_table[_edge_no][edge_no] = 2
-                if _check_two_graph(_edge_no, {edge_no: 0}) is False:
+                chain_table[node_no][_node_no] = chain_table[_node_no][node_no] = 2
+                if _check_two_graph(_node_no, {node_no: 0}) is False:
                     return 'No'
     return 'Yes'
 
 
-def _check_two_graph(target_edge, steps):
-    steps = {edge_no: step + 1 for edge_no, step in steps.items()}
-    for edge_no, step in steps.items():
-        if edge_no == target_edge and step % 2 == 1:
+def _check_two_graph(target_node, steps):
+    steps = {node_no: step + 1 for node_no, step in steps.items()}
+    for node_no, step in steps.items():
+        if node_no == target_node and step % 2 == 1:
             return False
     global chain_table
-    for _edge_no, step_sts in chain_table[target_edge].items():
+    for _node_no, step_sts in chain_table[target_node].items():
         if step_sts == 1:
-            chain_table[target_edge][_edge_no] = chain_table[_edge_no][target_edge] = 2
-            steps[target_edge] = 0
-            if _check_two_graph(_edge_no, steps) is False:
+            chain_table[target_node][_node_no] = chain_table[_node_no][target_node] = 2
+            steps[target_node] = 0
+            if _check_two_graph(_node_no, steps) is False:
                 return False
 
 
