8a4e370612de44218f612cd24c9547b5f5e07d3d
空久保充弘 <msorakubo@msn.com>
2015-10-14 18:36:01 +0900

    少しだけリファクタ

--- AlgorithmChallenge2_2_data_structure/6_two_graph_judge.py
+++ AlgorithmChallenge2_2_data_structure/6_two_graph_judge.py
@@ -12,13 +12,11 @@ def solve(n, chains):
     global chain_table
     chain_table = {i: {j: 0 for j in range(n)} for i in range(n)}
     for first, second in chains:
-        chain_table[first][second] = 1
-        chain_table[second][first] = 1
+        chain_table[first][second] = chain_table[second][first] = 1
     for edge_no, chain_row in chain_table.items():
         for _edge_no, step_sts in chain_row.items():
             if step_sts == 1:
-                chain_table[edge_no][_edge_no] = 2
-                chain_table[_edge_no][edge_no] = 2
+                chain_table[edge_no][_edge_no] = chain_table[_edge_no][edge_no] = 2
                 if _check_two_graph(_edge_no, {edge_no: 0}) is False:
                     return 'No'
     return 'Yes'
@@ -32,8 +30,7 @@ def _check_two_graph(target_edge, steps):
     global chain_table
     for _edge_no, step_sts in chain_table[target_edge].items():
         if step_sts == 1:
-            chain_table[target_edge][_edge_no] = 2
-            chain_table[_edge_no][target_edge] = 2
+            chain_table[target_edge][_edge_no] = chain_table[_edge_no][target_edge] = 2
             steps[target_edge] = 0
             if _check_two_graph(_edge_no, steps) is False:
                 return False
