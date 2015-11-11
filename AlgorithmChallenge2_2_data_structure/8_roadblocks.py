4a8320d1621d59247fc90116fcfdcf9f38e0a076
空久保充弘 <msorakubo@msn.com>
2015-11-11 14:49:00 +0900

    正しくheapできてなかったので修正

--- AlgorithmChallenge2_2_data_structure/8_roadblocks.py
+++ AlgorithmChallenge2_2_data_structure/8_roadblocks.py
@@ -5,15 +5,15 @@ from heapq import heappush, heappop
 
 def solve(node_num, paths):
     """
-    まずedgesから_mapを作る
+    まずpathsから_mapを作る
     hはheapq
     dpはnodeをkeyとして、２つの値をlistで持つ。defaultは[99999, 99999]
-    startはnode=1なので、nodeと距離を(1, 距離)でhに登録していく
+    startはnode=1なので、距離とnodeを(距離, 1(node))でhに登録していく
     hから取り出した値は、dpの両方の距離と比較して同じ、もしくは大きかったらスキップ、小さい場合はdpの大きい値と入れ替える
     全て探索した後、goalの２番目に大きい値を返す
 
     :param int node_num:
-    :param list edges:
+    :param list paths:
     :return int:
     """
     _map = {}
@@ -24,28 +24,28 @@ def solve(node_num, paths):
         _map.setdefault(path[1], []).append((path[0], path[2]))
     for i in range(node_num):
         dp[i + 1] = [99999, 99999]
-    heappush(h, (1, 0))
+    heappush(h, (0, 1))
 
     def _get_next():
         while True:
             if len(h) == 0:
                 return None
             step = heappop(h)
-            if (step[1] == dp[step[0]][0] or step[1] == dp[step[0]][1] or
-                    (step[1] > dp[step[0]][0] and step[1] > dp[step[0]][1])):
+            if (step[0] == dp[step[1]][0] or step[0] == dp[step[1]][1] or
+                    (step[0] > dp[step[1]][0] and step[0] > dp[step[1]][1])):
                 continue
-            if dp[step[0]][0] == dp[step[0]][1]:
-                del dp[step[0]][1]
+            if dp[step[1]][0] == dp[step[1]][1]:
+                del dp[step[1]][1]
             else:
-                dp[step[0]].remove(max(dp[step[0]]))
-            dp[step[0]].append(step[1])
+                dp[step[1]].remove(max(dp[step[1]]))
+            dp[step[1]].append(step[0])
             return step
 
     def _solve(step):
-        if step[0] not in _map:
+        if step[1] not in _map:
             return
-        for _path in _map[step[0]]:
-            heappush(h, (_path[0], step[1] + _path[1]))
+        for _path in _map[step[1]]:
+            heappush(h, (step[0] + _path[1], _path[0]))
 
     while True:
         _next = _get_next()
