14e8fb8b78316c88f32523c46f3ec54008a15720
空久保充弘 <msorakubo@msn.com>
2015-11-12 15:33:56 +0900

    バグ修正

--- AlgorithmChallenge2_2_data_structure/9_conscription.py
+++ AlgorithmChallenge2_2_data_structure/9_conscription.py
@@ -7,20 +7,30 @@ _MAX_COST = 10000
 
 def solve(women_num, men_num, relation_num, relations):
     h = []
-    for relation in relations:
-        heappush(h, (_MAX_COST - relation[2], 'm' + str(relation[0]), 'w' + str(relation[1])))
+    heappush(h, (_MAX_COST, 0))
     cost = 0
-    note = set()
+    team = set()
     while len(h) > 0:
-        _cost, man_no, woman_no = heappop(h)
-        if man_no in note and woman_no in note:
+        _cost, person_no = heappop(h)
+        if person_no in team:
             continue
-        if man_no not in note and woman_no not in note:
-            cost += _MAX_COST
+        team.add(person_no)
         cost += _cost
-        note.add(man_no)
-        note.add(woman_no)
-    return cost + _MAX_COST * (women_num + men_num - len(note))
+        if len(team) == women_num + men_num:
+            break
+        for man_no, woman_no, relation_num in relations:
+            if man_no in team and men_num + woman_no + 1 in team:
+                continue
+            if man_no in team:
+                heappush(h, (_MAX_COST - relation_num, men_num + woman_no + 1))
+                continue
+            if men_num + woman_no + 1 in team:
+                heappush(h, (_MAX_COST - relation_num, man_no))
+                continue
+            heappush(h, (_MAX_COST, man_no))
+            heappush(h, (_MAX_COST, men_num + woman_no + 1))
+    cost += (women_num + men_num - len(team)) * _MAX_COST
+    return cost
 
 if __name__ == '__main__':
     print(datetime.now())
@@ -28,4 +38,5 @@ if __name__ == '__main__':
                                    (4, 2, 2104), (2, 2, 781)]))
     print(54223 == solve(5, 5, 10, [(2, 4, 9820), (3, 2, 6236), (3, 1, 8864), (2, 4, 8326), (2, 0, 5156), (2, 0, 1463),
                                     (4, 1, 2439), (0, 4, 4373), (3, 4, 8889), (2, 4, 3133)]))
+    print(37989 == solve(2, 2, 3, [(0, 0, 1000), (0, 1, 10), (1, 1, 1001)]))
     print(datetime.now())
