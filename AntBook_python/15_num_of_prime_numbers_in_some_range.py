48d13c5200db3f6b1a006c6ee52e92b2966aeb74
空久保充弘 <msorakubo@msn.com>
2016-03-16 19:10:27 +0900

    ルート b よりも a が小さい場合、i が素数の場合でも素数ではないと判断されてしまうという誤りがあったので修正

--- AntBook_python/15_num_of_prime_numbers_in_some_range.py
+++ AntBook_python/15_num_of_prime_numbers_in_some_range.py
@@ -8,13 +8,15 @@ def solve(a, b):
         return 0
     prime_judges = [True for _ in range(b - a)]
     sq = floor(sqrt(b))
-    for i in range(2, sq + 1):
+    for i in range(2, sq + 1):  # todo 1 - ルートb までの list を別にもち、そこでも素数かどうかを管理して次の行を早める
         j = (a // i) * i + (0 if a % i == 0 else i)
+        if a // i <= 1:  # ルート b よりも a が小さい場合、i が素数の場合でも素数ではないと判断されてしまうため
+            j += i
         while a <= j < b:
             prime_judges[j - a] = False
             j += i
     result = 0
-    for judge in prime_judges:
+    for i, judge in enumerate(prime_judges):
         if judge is True:
             result += 1
     return result
@@ -24,4 +26,6 @@ if __name__ == '__main__':
     print(datetime.now())
     print(solve(22, 37) == 3)
     print(solve(22801763489, 22801787297) == 1000)
+    print(solve(999999000000, 1000000000000))
+    print(solve(2, 30) == 10)  # 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
     print(datetime.now())
