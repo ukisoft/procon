33e3ba4c68340c435c33e8e46967e7a95ef92b58
空久保充弘 <msorakubo@msn.com>
2016-07-11 10:42:35 +0900

    動的計画法で解き直した

--- AntBook_python/19_bribe_the_prisoners.py
+++ AntBook_python/19_bribe_the_prisoners.py
@@ -4,46 +4,26 @@ from datetime import datetime
 
 def solve(p: int, q: int, a: list):
     """
-    最後に空室にした際に、支払わなければならないコインの数を計算する
-    最も支払いが少ない部屋が最後に支払われるべき部屋となる
-    上記で計算した最少支払コイン数を result に追加する
-    上記の部屋が空室ではないという条件で、再度支払わなければならないコインの数を計算する
-    これを空室がなくなるまで続け、最後に result を返す
+    動的計画法
 
     :param p: 全ての部屋の数
     :param q: 空室にする部屋の数
     :param a: 空室にする部屋の位置
     :return: 最小支払いコイン数
     """
-    a.sort()
-    total_coin = 0
-    coins = [_get_coin(a, i, p) for i, _ in enumerate(a)]
-    while a:
-        target_i = coins.index(min(coins))
-        total_coin += coins[target_i]
-        a.pop(target_i)
-        if target_i != 0:
-            coins[target_i - 1] = _get_coin(a, target_i - 1, p)
-        if target_i != len(a):
-            coins[target_i + 1] = _get_coin(a, target_i, p)
-        coins.pop(target_i)
-    return total_coin
-
-
-def _get_coin(a, i, p):
-    if i == 0 and i == len(a) - 1:
-        left_coin = a[i] - 1
-        right_coin = p - a[i]
-    elif i == 0:
-        left_coin = a[i] - 1
-        right_coin = a[i + 1] - a[i] - 1
-    elif i == len(a) - 1:
-        left_coin = a[i] - a[i - 1] - 1
-        right_coin = p - a[i]
-    else:
-        left_coin = a[i] - a[i - 1] - 1
-        right_coin = a[i + 1] - a[i] - 1
-    return left_coin + right_coin
+    _a = [0]
+    _a.extend(a + [p + 1])
+    dp = {}
+    for _q in range(q + 1):
+        dp.setdefault(_q, {})[_q + 1] = 0
+    for w in range(2, q + 2):
+        for i in range(0, q + 2 - w):
+            j = w + i
+            t = 100000
+            for k in range(i + 1, j):
+                t = min(t, dp[i][k] + dp[k][j])
+            dp[i][j] = t + _a[j] + _a[i] - 2
+    return dp[0][q + 1]
 
 
 if __name__ == '__main__':
