d81b21ec6e8e24f1c7495092079578326452c043
空久保充弘 <msorakubo@msn.com>
2016-10-21 01:40:35 +0900

    refactor

--- AntBook_python/21_lower_bound.py
+++ AntBook_python/21_lower_bound.py
@@ -12,10 +12,7 @@ def _solve(target_list, num):
     l = len(target_list)
     if l == 1:
         return 0 if target_list[0] >= num else 1
-    if l % 2 == 1:
-        i = int((l + 1) / 2 - 1)
-    else:
-        i = int(l / 2 - 1)
+    i = l // 2
     if target_list[i] >= num:
         return _solve(target_list[0:i], num)
     return _solve(target_list[i + 1:], num) + i + 1
@@ -25,4 +22,6 @@ if __name__ == '__main__':
     print(datetime.now())
     print(solve(5, [2, 3, 3, 5, 6], 3) == 1)
     print(solve(10, [0, 0, 1, 2, 5, 10, 11, 11, 15, 20], 2) == 3)
+    print(solve(10, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0) == 0)
+    print(solve(10, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1) == 10)
     print(datetime.now())
